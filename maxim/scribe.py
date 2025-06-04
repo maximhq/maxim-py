62db12f0e028227dac3d4a5569d4e504bf320976
Akshay Deo <akshay@akshaydeo.com>
2025-06-04 10:16:55 +0530

    fix: Fixes default debug level of the SDK

--- maxim/scribe.py
+++ maxim/scribe.py
@@ -4,56 +4,76 @@ import logging
 # When users set the level with logging.getLogger('maxim').setLevel(logging.DEBUG)
 # this logger will respect that level setting
 
+_scribe_instance = None
 
-class Scribe():
-    
+
+class Scribe:
+    """
+    Scribe logger wrapper for maxim.
+    Log level is managed externally via set_level or the standard logging API.
+    By default, the logger uses the global logging configuration.
+    """
+
     def __init__(self, name):
+        self.name = name
         self.disable_internal_logs = True
-        self.logger = logging.getLogger(name)
+        self.logger = logging.getLogger(name)        
         
+
+    def _should_log(self, msg):
+        return not (
+            self.disable_internal_logs
+            and isinstance(msg, str)
+            and msg.startswith("[Internal]")
+        )
+
     def debug(self, msg, *args, **kwargs):
-        if self.disable_internal_logs and msg.startswith("[Internal]"):
+        if not self._should_log(msg):
             return
         self.logger.debug(msg, *args, **kwargs)
-        
+
     def warning(self, msg, *args, **kwargs):
-        if self.disable_internal_logs and msg.startswith("[Internal]"):
+        if not self._should_log(msg):
             return
         self.logger.warning(msg, *args, **kwargs)
 
-    def error(self, msg, *args, **kwargs):
-        if self.disable_internal_logs and msg.startswith("[Internal]"):
+    def log(self, level, msg, *args, **kwargs):
+        if not self._should_log(msg):
             return
+        self.logger.log(level, msg, *args, **kwargs)
+
+    def silence(self):
+        self.logger.setLevel(logging.CRITICAL + 1)
+
+    def error(self, msg, *args, **kwargs):
         self.logger.error(msg, *args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
-        if self.disable_internal_logs and msg.startswith("[Internal]"):
+        if not self._should_log(msg):
             return
-        self.logger.info(msg, *args, **kwargs)    
-
-    def set_level(self, level):
-        self.logger.setLevel(level)
+        if self.get_level() > logging.INFO:
+            return
+        self.logger.info(msg, *args, **kwargs)
 
     def get_level(self):
-        return self.logger.getLevel()
-    
-    @property
-    def level(self):
         return self.logger.level
     
-    @level.setter
-    def level(self, level):
-        self.logger.level = level
-    
+    def set_level(self, level):
+        self.logger.setLevel(level)
+        
 
 def scribe():
-    logger = Scribe("maxim")    
-    return logger
-
-
-if scribe().level == logging.NOTSET:
-    print("\033[32m[MaximSDK] Using global logging level\033[0m")
-else:
-    print(
-        f"\033[32m[MaximSDK] Log level set to {logging.getLevelName(scribe().level)}.\nYou can change it by calling logging.getLogger('maxim').setLevel(newLevel)\033[0m"
-    )
+    global _scribe_instance
+    if _scribe_instance is None:
+        _scribe_instance = Scribe("maxim")        
+        if _scribe_instance.get_level() == logging.NOTSET:
+            print("\033[32m[MaximSDK] Using info logging level.\033[0m")
+            print(
+                "\033[32m[MaximSDK] For debug logs, set global logging level to debug logging.basicConfig(level=logging.DEBUG).\033[0m"
+            )     
+            _scribe_instance.set_level(logging.INFO)
+        else:
+            print(
+                f"\033[32m[MaximSDK] Log level set to {logging.getLevelName(_scribe_instance.get_level())}.\nYou can change it by calling logging.getLogger('maxim').setLevel(newLevel)\033[0m"
+            )
+    return _scribe_instance
--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-rm -rf build
-rm -rf maxim_py.egg-info
-export PATH=$PATH:.venv/bin
-source .venv/bin/activate
-python3 setup.py sdist bdist_wheel
-twine upload dist/*
