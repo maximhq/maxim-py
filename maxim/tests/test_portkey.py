5789a2025283b3291155c975b963b5bef926e83a
Akshay Deo <akshay@akshaydeo.com>
2025-06-06 19:26:50 +0530

    some test case fixes

--- maxim/tests/test_portkey.py
+++ maxim/tests/test_portkey.py
@@ -2,28 +2,23 @@ import unittest
 
 import portkey_ai
 
-from maxim import Config, Maxim
-from maxim.logger import LoggerConfig
+from maxim import Maxim
+from maxim.logger.openai.client import MaximOpenAIAsyncClient, MaximOpenAIClient
 from maxim.logger.portkey import instrument_portkey
-from maxim.logger.openai.client import MaximOpenAIClient, MaximOpenAIAsyncClient
 
-
-logger = Maxim(Config(api_key="a", base_url="https://example.com")).logger(
-    LoggerConfig(id="repo")
-)
+logger = Maxim().logger()
 
 
 class TestPortkeyIntegration(unittest.TestCase):
     def test_instrument_portkey_sync(self):
-        client = portkey_ai.Portkey(api_key="dummy")
+        client = portkey_ai.Portkey(api_key="/FGpTnLIT91fFT8XmIljONdo17aY",virtual_key="open-ai-virtual-57f8a1")
         instrument_portkey(client, logger)
         self.assertIsInstance(client.openai_client, MaximOpenAIClient)
+        response = client.chat.completions.create(messages=[{"role": "user", "content": "Tell me what is big bang theory in 100 words?"}], model="gpt-4o", max_tokens=1000)
+        print(response)
 
     def test_instrument_portkey_async(self):
-        client = portkey_ai.AsyncPortkey(api_key="dummy")
+        client = portkey_ai.AsyncPortkey(api_key="/FGpTnLIT91fFT8XmIljONdo17aY",virtual_key="open-ai-virtual-57f8a1")
         instrument_portkey(client, logger)
         self.assertIsInstance(client.openai_client, MaximOpenAIAsyncClient)
 
-
-if __name__ == "__main__":
-    unittest.main()
