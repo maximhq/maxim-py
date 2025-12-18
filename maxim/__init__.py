bdad7512eb53f95f3566bbe54eb541a35198253f
Samyabrata Maji <116789799+sammaji@users.noreply.github.com>
2025-12-18 19:58:28 +0530

    feat: adds a function for replacing variables from templates

--- maxim/__init__.py
+++ maxim/__init__.py
@@ -1,5 +1,6 @@
 from .logger import Logger, LoggerConfig
 from .maxim import Config, Maxim
 from .models.test_run import TestRunConfig
+from .utils import replace_variables
 
-__all__ = ["Maxim", "Config", "TestRunConfig", "Logger", "LoggerConfig"]
+__all__ = ["Maxim", "Config", "TestRunConfig", "Logger", "LoggerConfig", "replace_variables"]
--- maxim/utils/utils.py
+++ maxim/utils/utils.py
@@ -3,6 +3,7 @@ import random
 import socket
 import threading
 import time
+import re
 
 
 def create_cuid_generator():
@@ -40,3 +41,22 @@ def create_cuid_generator():
         return f"c{timestamp}{counter_str}{random_component}{fingerprint}"
 
     return generate_cuid
+
+
+def replace_variables(template: str, variables: dict[str, str]) -> str:
+    """
+    Replaces all the variables with their values.
+    If no value is provided, the variable will be kept as-is in the output.
+
+    Args:
+        template: The template string to replace variables in.
+        variables: The variables to replace.
+
+    Returns:
+        String with all variables replaced.
+
+    Examples:
+        replace_variables("Hello {{name}}!", {"name": "World"})
+    """
+    return re.sub(r'\{\{(\w+)\}\}', lambda m: variables.get(m.group(1), m.group(0)), template)
+
